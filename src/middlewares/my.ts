3ed43424030abf1ace1fbee7fea00fb1eb1fdfd1
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-28 13:08:15 -0300

    feat: criando middleware de tratamento

---
+++ src/middlewares/my.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/app-error";
+import { ZodError } from "zod";
+
+export function errorHandler(err: Error, req: Request, res: Response, _next : NextFunction) {
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({ message: err.message });
+    } else if (err instanceof ZodError) {
+        return res.status(400).json({ message: err.errors, issues: err.format() });
+    } else if (err instanceof Error) {
+        return res.status(500).json({ message: 'Erro interno no servidor!' });
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // IMPORTANDO MÓDULOS
 import express, { Request, Response } from 'express';
 import { routes } from './routes';
+import { errorHandler } from './middlewares/my';
 
 // DEFININDO VARIÁVEIS
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
